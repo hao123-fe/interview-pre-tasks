518b40aab042532ff01812b0ed019d9487e4462f
wj <593949622@qq.com>
2015-10-17 01:43:24 +0800

    for task-1 answer : authoer:wangjiejie

---
+++ wangjiejie-answer-at-here-for-task1/js/bdcount.js
@@ -0,0 +1,10 @@
+/**
+ * Created by admin on 2015/7/20.
+ */
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?3cd749e944ed3077ec6bc11adf352b6d";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
