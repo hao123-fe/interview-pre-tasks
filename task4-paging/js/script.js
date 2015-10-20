cfa1f582df7305a418b41e52e276758000a1001d
Yi-Ting Lin <yiting.lin1103@gmail.com>
2015-10-20 17:48:50 +0800

    Fix goToPage not working

--- task4-paging/js/script.js
+++ task4-paging/js/script.js
@@ -1,15 +1,13 @@
 var paging = function(selector) {
   if (!selector) return false;
+
   this.pageCount = 10;
   this.pageNumRange = 2;
-
-  var pagination, nowPage, nextPage, prevPage;
-  var selector = selector;
 
   this.goToPage = function(page) {
     if (!page) return false;
     var temp = getPagingTemp.bind(this, page);
-    $(pagination).html(temp);
+    $(selector).html(temp);
   }
 
   this.pageWillChange = function(now, next) {
@@ -21,6 +19,10 @@ var paging = function(selector) {
     nowPage = now;
     prevPage = prev;
   }
+
+  var nowPage = 5,
+      nextPage = 0,
+      prevPage = 0;
 
   function handleClick(event) {
     event.preventDefault();
@@ -46,8 +48,7 @@ var paging = function(selector) {
       temp = currentPage - this.pageNumRange - 1 > 1 ?
         '<a href="#">1</a>...' : '<a href="#">1</a>';
     }
-    for (var i = 1; i <= this.pageCount; i++) {
-      if (i == 1 || i == this.pageCount) continue;
+    for (var i = 2; i < this.pageCount; i++) {
       if (i >= currentPage - this.pageNumRange &&
           i <= currentPage + this.pageNumRange) {
         temp += i == currentPage ?
@@ -67,19 +68,10 @@ var paging = function(selector) {
   }
 
   function init() {
-    nowPage = 5;
-    prevPage = 0;
-    nextPage = 0;
-    pagination = document.getElementById(selector);
-
-    var temp = getPagingTemp.bind(this, nowPage);
-    $(pagination).html(temp);
-
-    $(pagination).on('click', 'a', handleClick.bind(this));
-    $(pagination).on('keyup change', 'input', handleInput.bind(this));
+    this.goToPage(nowPage);
+    $(selector).on('click', 'a', handleClick.bind(this));
+    $(selector).on('keyup change', 'input', handleInput.bind(this));
   }
 
   window.addEventListener('load', init.bind(this));
 };
-
-var a = new paging('pagination');
